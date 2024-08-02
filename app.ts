f8ab3a32927e6bf04a16139bfa11551d32694a50
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-08-02 22:18:25 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+let cars: string = 'subaru';
+
+// Test 1: Equality comparison (True)
+console.log("Is car == 'subaru'? I predict True.");
+console.log(cars == 'subaru'); // True
+
+// Test 2: Strict equality comparison (True)
+console.log("Is car === 'subaru'? I predict True.");
+console.log(cars === 'subaru'); // True
